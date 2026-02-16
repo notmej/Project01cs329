3da7407c02239880ca5133e3c6fef3d7df4551fd
notmej <yunonaagzamova@gmail.com>
2026-02-16 20:45:28 +0300

    fixed tests, now on Junit4

---
+++ test/project01/Project01Suite.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4Suite.java to edit this template
+ */
+package project01;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author yuna
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({project01.Project01Test.class, project01.ConvertTest.class})
+public class Project01Suite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
